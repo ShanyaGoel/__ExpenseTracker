b1f3768060f80ab3792cb95eb095e2bb56c1e272
ShanyaGoel <shanyagoel83@gmail.com>
2021-06-24 19:41:53 +0530

    Added transaction path

--- src/component/TransactionList.js
+++ src/component/TransactionList.js
@@ -1,18 +1,16 @@
 import React,{useContext} from 'react'
-import { GlobalContext  } from '../Context/GlobalState';
+import {Transaction} from './Transaction';
+import { GlobalContext} from '../Context/GlobalState';
 
 export const TransactionList = () => {
   const{transactions}  =useContext(GlobalContext);
 
-  console.log(context); 
+  
     return (
         <>
         <h3>History</h3>
       <ul  className="list">
-        
-         <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li> 
+        {transactions.map(transaction=>(<Transaction key={transaction.id} transaction ={transaction}/> ))}
       </ul>
       </>
     )
